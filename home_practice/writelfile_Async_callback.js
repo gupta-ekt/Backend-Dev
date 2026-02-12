eee00412d0b07d79d62df1aa4db2f0a061467f85
EKTA GUPTA <143394635+gupta-ekt@users.noreply.github.com>
2026-02-12 14:54:22 +0530

    Delete home_practice directory

--- home_practice/writelfile_Async_callback.js
+++
@@ -1,9 +0,0 @@
-const fspromises = require("fs").promises;
-
-fspromises.writeFile("example.txt", "utf-8", (data, err)=>{
-    if(err){
-        console.log("error in writing", err);
-        return;
-    }
-    console.log("data wrtiitemn", data);
-});
